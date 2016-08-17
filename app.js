b5147c57d803fa8004b3a58ba9db7deeb6f94392
Everaldo Gomes <everaldo.gomes@gmail.com>
2016-08-17 14:31:36 -0300

    Otimização do Exercício

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
-var app = angular.module('ListaTarefas',
+var app = angular.module('listaTarefas',
  [])
-  .controller('TarefasController',
-   function($scope){
-    $scope.id = 0;
+  .controller('TarefasController', function($scope){
+    var id = 0;
     $scope.tarefas = [];
-    $scope.novaTarefa = {
-      feita: false
-    };
 
-    $scope.atualizaCheckbox = function(tarefa){
-      var label = document.querySelector("#label_tarefa_" + tarefa.id);
-      if(tarefa.feita){
-        label.style.textDecoration = "line-through";
-      }
-      else{
-        label.style.textDecoration = "none";
-      }
+    function criaTarefa(){
+      return {
+        id: id++,
+        descricao: "",
+        feita: false
+      };
     }
 
+    $scope.tarefa = criaTarefa();
 
     $scope.adicionarTarefa = function(tarefa){
-      tarefa.id = $scope.id;
-      $scope.id = $scope.id + 1;
-      nova = {};
-      nova.id = tarefa.id;
-      nova.descricao = tarefa.descricao;
-      nova.feita = tarefa.feita;
-      $scope.tarefas.push(nova);
-      $scope.novaTarefa = {
-        feita: false,
-        descricao: ''
-      };
+      $scope.tarefas.push(angular.copy(tarefa));
+      $scope.tarefa = criaTarefa();
       $scope.formNovaTarefa.$setPristine();
     }
   });
